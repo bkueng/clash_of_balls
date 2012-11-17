de5ac31b071e57b5cde99cff1bc6d82bc79026eb
Beat Küng <beat-kueng@gmx.net>
2012-11-17 15:33:27 +0100

    + LevelPreviewer skeleton class

---
+++ src/com/android/game/clash_of_the_balls/menu/LevelPreviewer.java
@@ -0,0 +1,10 @@
+package com.android.game.clash_of_the_balls.menu;
+
+
+public class LevelPreviewer {
+	//use LevelLoader & GameField ...
+	
+	//use scrollable container view which contains MenuItem's ?
+	// -> use shader to hide container overflow
+	//one level = one MenuItem
+}
