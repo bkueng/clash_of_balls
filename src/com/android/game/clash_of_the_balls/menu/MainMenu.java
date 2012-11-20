ce6a76314cd394a86363e9bcd01a3d8192e5c043
Hans Hardmeier <harhans@ethz.ch>
2012-11-20 15:17:41 +0100

    + 4 Main Menu Buttons

--- src/com/android/game/clash_of_the_balls/menu/MainMenu.java
+++ src/com/android/game/clash_of_the_balls/menu/MainMenu.java
@@ -9,7 +9,12 @@ import com.android.game.clash_of_the_balls.game.Vector;
 
 public class MainMenu extends GameMenuBase {
 	
-	MenuItem m_test_button;
+	private String LOG_TAG="debug";
+	
+	MenuItem m_host_button;
+	MenuItem m_join_button;
+	MenuItem m_help_button;
+	MenuItem m_credits_button;
 	
 	public MainMenu(Font2D item_font, MenuBackground background
 			, float screen_width, float screen_height,TextureManager m_tex_manager) {
@@ -23,15 +28,36 @@ public class MainMenu extends GameMenuBase {
 		
 		
 		//add menu items
-		float button_width = size.x * 0.2f;
-		float button_height=0.394f*button_width;
-		m_menu_items.add(m_test_button = new MenuItemButton(
-				new Vector(pos.x+size.x / 2.f, pos.y+size.y / 2.f),
+		float button_width = size.x * 0.3f;
+		float button_height=0.3f*button_width;
+		float distanceButtons = screen_height/34.f;
+		
+		m_menu_items.add(m_host_button = new MenuItemButton(
+				new Vector(pos.x+size.x*2.f/ 3.f, pos.y+size.y*3.f/5.f),
 				new Vector(button_width, button_height), 
 				m_item_font,
 				m_tex_manager));
 		
+		m_menu_items.add(m_join_button = new MenuItemButton(
+				new Vector(pos.x+size.x*2.f/ 3.f,
+						pos.y+size.y*3.f/5.f-(button_height+distanceButtons)),
+				new Vector(button_width, button_height), 
+				m_item_font,
+				m_tex_manager));
 		
+		m_menu_items.add(m_help_button = new MenuItemButton(
+				new Vector(pos.x+size.x*2.f/ 3.f, 
+						pos.y+size.y*3.f/5.f-2*(button_height+distanceButtons)),
+				new Vector(button_width, button_height), 
+				m_item_font,
+				m_tex_manager));
+		
+		m_menu_items.add(m_credits_button = new MenuItemButton(
+				new Vector(pos.x+size.x*2.f/ 3.f, 
+						pos.y+size.y*3.f/5.f-3*(button_height+distanceButtons)),
+				new Vector(button_width, button_height), 
+				m_item_font,
+				m_tex_manager));
 	}
 
 	@Override
@@ -41,8 +67,16 @@ public class MainMenu extends GameMenuBase {
 
 	@Override
 	protected void onTouchUp(MenuItem item) {
-		if(item == m_test_button) {
+		if(item == m_join_button) {
 			//m_ui_change = UIChange.new_menu;
+		}else if(item == m_host_button){
+			
+		}else if(item == m_help_button){
+			
+		}else if(item == m_credits_button){
+			
+		}else{
+			Log.d(LOG_TAG,"No button pressed");
 		}
 	}
 
