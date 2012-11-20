d5d536c29097983c13a4d9feda7e245fe00893d8
Hans Hardmeier <harhans@ethz.ch>
2012-11-20 11:49:19 +0100

    ! Ratio for background

--- src/com/android/game/clash_of_the_balls/UIHandler.java
+++ src/com/android/game/clash_of_the_balls/UIHandler.java
@@ -55,7 +55,7 @@ public class UIHandler implements IDrawable, IMoveable, ITouchInput {
 		m_level_manager.loadLevels();
 		
 		m_main_menu_background = new MenuBackground(
-				m_tex_manager.get(R.raw.texture_main_menu_bg), (float)screen_width/(float)screen_height);
+				m_tex_manager.get(R.raw.texture_main_menu_bg),1600.f/960.f);
 		m_main_menu = new MainMenu(m_menu_item_font, m_main_menu_background
 				, screen_width, screen_height,m_tex_manager);
 		
