3d0e42c2691daa8de8f43b0c6e6587cccd81492f
Beat Küng <beat-kueng@gmx.net>
2012-11-27 17:34:11 +0100

    ! remove unnecessary duplicate code in TextureHelper TODO: - remove all fonts from uiHandler + menu constructors + attributes (GameMenuBase, ...) 	-> create font inside MenuItemButton ... 	-> add static final int font_color=0x... in GameMenuBase -> use it in MenuItemButton constructor
    - remove model mat code in font drawing

--- src/com/android/game/clash_of_the_balls/helper/TextureHelper.java
+++ src/com/android/game/clash_of_the_balls/helper/TextureHelper.java
@@ -12,45 +12,21 @@ public class TextureHelper
 		
 	public static int loadTexture(final Context context, final int resourceId)
 	{
-		final int[] textureHandle = new int[1];
+		final int textureHandle;
 		
-		GLES20.glGenTextures(1, textureHandle, 0);
-		
-		if (textureHandle[0] != 0)
-		{
-			final BitmapFactory.Options options = new BitmapFactory.Options();
-			options.inScaled = false;	// No pre-scaling
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inScaled = false;	// No pre-scaling
 
-			// Read in the resource
-			final Bitmap loaded_bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId, options);
-			//flip it upside down (android uses different coord system than opengl)
-			Matrix flip = new Matrix();
-			flip.postScale(1f, -1f);
-			final Bitmap bitmap = Bitmap.createBitmap(loaded_bitmap, 0, 0
-					, loaded_bitmap.getWidth(), loaded_bitmap.getHeight(), flip, true);
-			
-			// Bind to the texture in OpenGL
-			GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
-			
-			// Set filtering
-			GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
-			GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
-			
-			// Load the bitmap into the bound texture.
-			GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
-			
-			// Recycle the bitmap, since its data has been loaded into OpenGL.
-			bitmap.recycle();	
-			
-			loaded_bitmap.recycle();
-		}
+		// Read in the resource
+		final Bitmap loaded_bitmap = BitmapFactory.decodeResource(
+				context.getResources(), resourceId, options);
+
+		textureHandle = loadTextureFromBitmap(loaded_bitmap);
+
+		// Recycle the bitmap, since its data has been loaded into OpenGL.
+		loaded_bitmap.recycle();
 		
-		if (textureHandle[0] == 0)
-		{
-			throw new RuntimeException("Error loading texture.");
-		}
-		
-		return textureHandle[0];
+		return textureHandle;
 	}
 	
 	public static int loadTextureFromBitmap(Bitmap bitmap) {
