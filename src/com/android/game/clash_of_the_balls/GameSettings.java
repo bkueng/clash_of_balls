8a99d16f72c6b4c85b555ec0ee735e0daf712a1d
Beat Küng <beat-kueng@gmx.net>
2012-11-29 21:06:57 +0100

    + GameSettings: add a debug variable (set to false)

--- src/com/android/game/clash_of_the_balls/GameSettings.java
+++ src/com/android/game/clash_of_the_balls/GameSettings.java
@@ -6,6 +6,10 @@ package com.android.game.clash_of_the_balls;
  *
  */
 public class GameSettings {
+	public static final boolean debug = false; //debugging variable
+						//this allows us some special things for testing if true
+						//eg starting a game without any connected clients
+	
 	public String user_name="";
 	
 	public boolean is_host=false;
