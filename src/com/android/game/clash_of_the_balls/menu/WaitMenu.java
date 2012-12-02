0f1ee554e823cecde883b6c7d2d715b96453b926
Beat Küng <beat-kueng@gmx.net>
2012-12-02 18:55:42 +0100

    ! refactor: use GameMenuInGame base class for menus that need network error checking

--- src/com/android/game/clash_of_the_balls/menu/WaitMenu.java
+++ src/com/android/game/clash_of_the_balls/menu/WaitMenu.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.android.game.clash_of_the_balls.Font2D;
 import com.android.game.clash_of_the_balls.GameSettings;
 import com.android.game.clash_of_the_balls.TextureManager;
+import com.android.game.clash_of_the_balls.game.GameMenuInGame;
 import com.android.game.clash_of_the_balls.game.Vector;
 import com.android.game.clash_of_the_balls.game.event.Event;
 import com.android.game.clash_of_the_balls.network.NetworkClient;
@@ -15,27 +16,18 @@ import com.android.game.clash_of_the_balls.network.Networking.ConnectedClient;
 import com.android.game.clash_of_the_balls.Font2D.Font2DSettings;
 import com.android.game.clash_of_the_balls.UIHandler.UIChange;
 
-public class WaitMenu extends GameMenuBase {
+public class WaitMenu extends GameMenuInGame {
 
 	private static final String LOG_TAG = "WaitMenu";
-
-	private GameSettings m_settings;
-	private TextureManager m_tex_manager;
-	
-	private PopupMsg m_error_popup = null;
 	
 	private Font2DSettings m_list_item_font_settings;
-	private Font2DSettings m_font_settings;
 
 	private MenuItemButton m_start_button;
 	private MenuItemButton m_cancel_button;
 	
 	private MenuItemList m_client_list;
 	private float m_item_height;
-	
-	private Networking m_networking;
-	private NetworkClient m_network_client;
-	
+
 
 	public WaitMenu(MenuBackground background,
 			float screen_width, float screen_height
@@ -43,19 +35,15 @@ public class WaitMenu extends GameMenuBase {
 			, Context context, Font2D.Font2DSettings font_settings
 			, Networking networking
 			, NetworkClient network_client) {
-		super(background, context);
+		
+		super(background, context, settings, networking
+				, network_client, tex_manager, font_settings);
 
 		Vector pos = new Vector(0.f, 0.f);
 		Vector size = new Vector(screen_width, screen_height);
 
 		if (m_background != null)
 			m_background.getViewport(screen_width, screen_height, pos, size);
-
-		m_settings = settings;
-		m_font_settings = font_settings;
-		m_tex_manager = tex_manager;
-		m_networking = networking;
-		m_network_client = network_client;
 
 		// add menu items
 		float button_width = size.x * 0.35f;
@@ -89,13 +77,10 @@ public class WaitMenu extends GameMenuBase {
 	}
 	
 	public void move(float dsec) {
-		if(m_error_popup != null) {
-			//check for button pressed
-			if(m_error_popup.UIChange() == UIChange.POPUP_RESULT_BUTTON1) {
-				m_ui_change = UIChange.GAME_ABORT;
-				m_error_popup = null;
-			}
-		} else {
+		
+		super.move(dsec);
+		
+		if(m_error_popup == null) {
 			//update the connected clients
 			boolean is_self_connected=false; //is our own name on the list?
 			//remove old ones
@@ -133,53 +118,14 @@ public class WaitMenu extends GameMenuBase {
 				}
 			}
 			
-			//check for game start signal if we are not the game creator
-			if(!m_settings.is_host) {
-				m_network_client.handleReceive();
-				Event e;
-				while((e = m_network_client.peekNextEvent()) != null) {
-					if(e.type == Event.type_game_info) {
-						onGameStart();
-						m_ui_change = UIChange.GAME_START_CLIENT;
-						break;
-					} else {
-						Log.w(LOG_TAG, "we received an unwanted game event from the server (type="
-							+ (int)e.type+"). We ignore it");
-						m_network_client.getNextEvent(); //discard event
-					}
-				}
-			}
-			
 			m_start_button.enable(m_settings.is_host
 					&& (m_client_list.itemCount() > 1 || GameSettings.debug)
 					&& is_self_connected);
 			
-			handleNetworkError(m_network_client.getNetworkError());
 		}
 	}
 	
-	private void handleNetworkError(AllJoynErrorData data) {
-		if(data != null) {
-			//this is bad: here it's very difficult to recover, so we 
-			//show a message to the user and abort the game
-			
-			switch(data.error) {
-			case CONNECT_ERROR:
-			case JOIN_SESSION_ERROR:
-			case SEND_ERROR:
-			case BUS_EXCEPTION:
-				m_settings.popup_menu = m_error_popup = new PopupMsg(m_activity_context 
-						, m_tex_manager, m_settings.m_screen_width
-						, m_settings.m_screen_height 
-						, m_font_settings.m_typeface, m_font_settings.m_color
-						, "Error", Networking.getErrorMsgMultiline(data.error), "Ok");
-				m_ui_change = UIChange.POPUP_SHOW;
-			}
-			
-		}
-	}
-	
-	private void onGameStart() {
+	protected void onGameStart() {
 		m_settings.game_statistics.resetGameStatistics();
 	}
 	
