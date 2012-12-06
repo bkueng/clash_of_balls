bf8693d66c4ea8af5f89557ae083e5100b65784c
Beat Küng <beat-kueng@gmx.net>
2012-12-06 19:19:05 +0100

    ! forgot one eps

--- src/com/android/game/clash_of_the_balls/game/GameBase.java
+++ src/com/android/game/clash_of_the_balls/game/GameBase.java
@@ -297,7 +297,7 @@ public abstract class GameBase {
 									float beta = player_a.newPosition().x - player_a.pos().x + player_b.newPosition().x - player_b.pos().x;
 									float delta = player_a.newPosition().y - player_a.pos().y + player_b.newPosition().y - player_b.pos().y;
 									
-									float rads = ((player_a.m_radius + player_b.m_radius + EPS) * (player_a.m_radius + player_b.m_radius + eps));
+									float rads = ((player_a.m_radius + player_b.m_radius + EPS) * (player_a.m_radius + player_b.m_radius + EPS));
 
 									float a = (beta * beta + delta * delta);
 									float b = 2.f * (alpha * beta + gamma * delta);
