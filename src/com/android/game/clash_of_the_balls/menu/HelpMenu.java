8c82a3e773f5beea4f8e3d9c3cd849283e29375b
Hans Hardmeier <harhans@ethz.ch>
2012-12-08 00:30:31 +0100

    + New UI for HelpMenu

--- src/com/android/game/clash_of_the_balls/menu/HelpMenu.java
+++ src/com/android/game/clash_of_the_balls/menu/HelpMenu.java
@@ -1,6 +1,11 @@
 package com.android.game.clash_of_the_balls.menu;
 
+import java.util.ArrayList;
+
 import android.content.Context;
+import android.text.method.ArrowKeyMovementMethod;
+import android.util.Log;
+import android.view.MotionEvent;
 
 import com.android.game.clash_of_the_balls.Font2D;
 import com.android.game.clash_of_the_balls.GameSettings;
@@ -9,94 +14,172 @@ import com.android.game.clash_of_the_balls.TextureManager;
 import com.android.game.clash_of_the_balls.Font2D.Font2DSettings;
 import com.android.game.clash_of_the_balls.Font2D.TextAlign;
 import com.android.game.clash_of_the_balls.R.raw;
+import com.android.game.clash_of_the_balls.game.RenderHelper;
 import com.android.game.clash_of_the_balls.game.Vector;
+import com.android.game.clash_of_the_balls.menu.MenuItemArrow.ArrowType;
 import com.android.game.clash_of_the_balls.network.NetworkClient;
 
 public class HelpMenu extends GameMenuBase {
 
 	private TextureManager m_tex_manager;
-	
-	private MenuItemStringMultiline m_first_label;
-	private MenuItemImg m_first_img;
-	private MenuItemStringMultiline m_second_label;
-	private MenuItemImg m_second_img;
-	private MenuItemStringMultiline m_third_label;
-	private MenuItemImg m_third_img;
-	
-	public HelpMenu(MenuBackground background
-			, float screen_width, float screen_height
-			, TextureManager tex_manager, Context context
-			, Font2D.Font2DSettings font_settings, int label_font_color
-			, GameSettings settings
-			, NetworkClient network_client) {
-		super(background,context);
-		
+	private Font2D.Font2DSettings m_label_font_settings;
+
+	float m_screen_width;
+	float m_screen_height;
+
+	private int m_curr_page = 0;
+	private int m_num_pages = 0;
+
+	private MenuItemArrow m_button_next;
+	private MenuItemArrow m_button_prev;
+
+	private boolean m_left_visible = false;
+	private boolean m_right_visible = false;
+
+	private Vector pos;
+	private Vector size;
+
+	private float img_width;
+	private float img_height;
+
+	private float text_width;
+	private float text_height;
+
+	private float offset_x;
+	private float offset_y;
+
+	ArrayList<ArrayList<MenuItem>> m_pages = new ArrayList<ArrayList<MenuItem>>();
+
+	public HelpMenu(MenuBackground background, float screen_width,
+			float screen_height, TextureManager tex_manager, Context context,
+			Font2D.Font2DSettings font_settings, int label_font_color,
+			GameSettings settings, NetworkClient network_client) {
+		super(background, context);
+
+		m_screen_height = screen_height;
+		m_screen_width = screen_width;
+		pos = new Vector(0.f, 0.f);
+		size = new Vector(m_screen_width, m_screen_height);
+		img_width = size.x * 0.65f;
+		img_height = .5f * img_width;
+		text_height = 0.4f * img_width;
+		text_width = 1.5f * img_width;
+		offset_x = size.y * 0.1f;
+		offset_y = offset_x * 0.5f;
+
 		m_tex_manager = tex_manager;
-		
-		Vector pos=new Vector(0.f, 0.f);
-		Vector size=new Vector(screen_width, screen_height);
-		
-		Font2D.Font2DSettings label_font_settings 
-		= new Font2D.Font2DSettings(font_settings.m_typeface,
-			TextAlign.RIGHT, label_font_color);
-		
-		//add menu items
-		float label_width = size.x * 0.35f;
-		float button_height=0.25f*label_width;
-		float label_height = 2.f*button_height;
-		
-		
-		float offset_x = size.y*0.1f;
-		float offset_y = offset_x*0.5f;
-		
-		m_menu_items.add(m_first_label = new MenuItemStringMultiline(
-				new Vector(pos.x + offset_x
-						, pos.y + size.y - label_height-offset_y),
-				new Vector(1.2f*label_width, label_height),
-				label_font_settings, " Push the others\ninto the holes", m_tex_manager));
-		
-		m_menu_items.add(m_first_img = new MenuItemImg(
-				new Vector(pos.x + offset_x+label_width+3*offset_x
-						, pos.y + size.y - label_height-offset_y),
-				new Vector(label_width, label_height),
-				m_tex_manager, R.raw.img_first));
-		
-		m_menu_items.add(m_second_label = new MenuItemStringMultiline(
-				new Vector(pos.x + offset_x+label_width+offset_x
-						, pos.y + size.y -2*(label_height)-offset_y),
-				new Vector(1.5f*label_width, label_height),
-				 new Font2D.Font2DSettings(font_settings.m_typeface,
-							TextAlign.LEFT, label_font_color), "Control your ball\n moving your phone!", m_tex_manager));
-		
-		m_menu_items.add(m_second_img = new MenuItemImg(
-				new Vector(pos.x + offset_x
-						, pos.y + size.y - 2*(label_height)-offset_y),
-				new Vector(label_width, label_height),
-				m_tex_manager, R.raw.img_second));
-		m_menu_items.add(m_third_label = new MenuItemStringMultiline(
-				new Vector(pos.x + 2*offset_x
-						, pos.y + size.y -3*(label_height)-offset_y),
-				new Vector(label_width, label_height),
-				label_font_settings, " Watch out\nfor Walls", m_tex_manager));
-		
-		m_menu_items.add(m_third_img = new MenuItemImg(
-				new Vector(pos.x + offset_x+label_width+3*offset_x
-						, pos.y + size.y - 3*(label_height)-offset_y),
-				new Vector(label_width, label_height),
-				m_tex_manager, R.raw.img_third));
+
+		m_label_font_settings = new Font2D.Font2DSettings(
+				font_settings.m_typeface, TextAlign.CENTER, label_font_color);
+
+		addPage("Push the others into\n the holes", R.raw.img_first);
+
+		addPage("Control your ball\n balancing your phone!", R.raw.img_second);
+
+		addPage(" Watch out\nfor Walls", R.raw.img_third);
+
+		m_button_next = new MenuItemArrow(new Vector(pos.x + size.x / 2
+				+ img_width / 2, pos.y + offset_y), new Vector(text_height / 2,
+				text_height / 2), m_tex_manager, ArrowType.RIGHT);
+
+		m_button_prev = new MenuItemArrow(new Vector(pos.x + offset_x, pos.y
+				+ offset_y), new Vector(text_height / 2, text_height / 2),
+				m_tex_manager, ArrowType.LEFT);
+
+		handlePageChanged();
+	}
+
+	private void handlePageChanged() {
+		if (m_curr_page > 0) {
+			m_left_visible = true;
+		} else {
+			m_left_visible = false;
+		}
+		if (m_curr_page < m_num_pages - 1) {
+			m_right_visible = true;
+		} else {
+			m_right_visible = false;
+		}
+		m_menu_items = m_pages.get(m_curr_page);
+
+		Log.d("debug", "Current Page:" + String.valueOf(m_curr_page));
+	}
+
+	private void addPage(String text, int raw_image) {
+		ArrayList<MenuItem> tmp_page = new ArrayList<MenuItem>();
+
+		tmp_page.add(new MenuItemStringMultiline(new Vector(pos.x + size.x / 2
+				- text_width / 2, pos.y + size.y - text_height - offset_y),
+				new Vector(text_width, text_height), m_label_font_settings,
+				text, m_tex_manager));
+
+		tmp_page.add(new MenuItemImg(new Vector(pos.x + size.x / 2 - img_width
+				/ 2, pos.y + offset_y), new Vector(img_width, img_height),
+				m_tex_manager, raw_image));
+
+		m_pages.add(tmp_page);
+		m_num_pages++;
+
+	}
+
+	public void draw(RenderHelper renderer) {
+		if (m_background != null)
+			m_background.draw(renderer);
+		if (m_left_visible) {
+			m_button_prev.draw(renderer);
+		}
+		if (m_right_visible)
+			m_button_next.draw(renderer);
+		for (int i = 0; i < m_menu_items.size(); ++i)
+			m_menu_items.get(i).draw(renderer);
+	}
+
+	@Override
+	public void onTouchEvent(float x, float y, int event) {
+		super.onTouchEvent(x, y, event);
+		if (event == MotionEvent.ACTION_DOWN) {
+			if (m_button_prev.isInside(x, y) && m_left_visible) {
+				m_button_prev.onTouchDown(x, y);
+				onTouchDown(m_button_prev);
+			}
+			if (m_button_next.isInside(x, y) && m_right_visible) {
+				m_button_next.onTouchDown(x, y);
+				onTouchDown(m_button_next);
+			}
+		} else if (event == MotionEvent.ACTION_UP) {
+			if (m_button_prev.isInside(x, y) && m_left_visible) {
+				m_button_prev.onTouchUp(x, y);
+				onTouchUp(m_button_prev);
+			}
+			if (m_button_next.isInside(x, y) && m_right_visible) {
+				m_button_next.onTouchUp(x, y);
+				onTouchUp(m_button_next);
+			}
+		}
 	}
 
 	@Override
 	protected void onTouchDown(MenuItem item) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void onTouchUp(MenuItem item) {
-		// TODO Auto-generated method stub
-		
+		if (item == m_button_next) {
+			nextPage();
+		} else if (item == m_button_prev) {
+			previousPage();
+		}
+
 	}
 
+	public void previousPage() {
+		m_curr_page = (--m_curr_page) % m_num_pages;
+		handlePageChanged();
+	}
+
+	public void nextPage() {
+		m_curr_page = (++m_curr_page) % m_num_pages;
+		handlePageChanged();
+	}
 
 }
